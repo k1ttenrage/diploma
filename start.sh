184b3b5c3629911bdd88a6ea26fb390f84b481f4
kittanrage <90337895+kittanrage@users.noreply.github.com>
2023-05-11 06:54:25 +0300

    final bash

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-npm run dev -- --https --host | egrep -o 'https?://[0-9][^ ]+/' -m1 | xargs open
+if npm run dev -- --https --host | egrep -o 'https?://[0-9][^ ]+/' -m1 | xargs open
+then
+    echo "fine"
+else
+    npm install --force
+fi 
